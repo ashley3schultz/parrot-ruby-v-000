d687e5acc3fc13a2a14df614cc052663e6bc6c6f
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-16 21:18:14 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,3 +1,11 @@
 def parrot(phrase = "Squawk!")
   puts "#{phrase}"
 end
+
+def parrot(sound = "Squawk!")
+  puts "#{sound}"
+end
+
+def greeting(name = "Ruby programmer")
+  puts "Hello, #{name}"
+end
